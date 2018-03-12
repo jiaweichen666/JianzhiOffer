6bee76c42b83584ffc2d903f74d8b10a4d0f3792
jiaweichen666 <1096448779@qq.com>
2018-03-12 20:49:36 +0800

    18 commit.

---
+++ src/p23EntryNodeInListLoop/Main.java
@@ -0,0 +1,17 @@
+package p23EntryNodeInListLoop;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        ListNode[] listNodes = new ListNode[6];
+        for (int i = 0; i < 6 ; i++) {
+            listNodes[i] = new ListNode(i + 1);
+        }
+        for (int i = 0; i < 5 ; i++) {
+            listNodes[i].next = listNodes[i + 1];
+        }
+        listNodes[5].next = listNodes[2];
+        ListNode startNode = solution.entryNodeofList(listNodes[0]);
+        System.out.println(startNode.val);
+    }
+}
